29c5180206b4da7df2bff8378b694cc9dcbf50a3
vinutha91 <vinu.raju@gmail.com>
2019-08-05 12:45:28 +0530

    [CC-1106] | Login | Added user details

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 console.log("Hello, welcome to Git Training!");
 
 console.log('These are my changes to cloned repository');
+console.log('These are my changes to cloned repository');
+
+console.log('Thirs change');
+console.log('These are my changes to cloned repository');
+
+console.log('Thirs change');
+console.log('These are my changes to cloned repository');
+
+console.log('Thirs change');
+console.log('These are my changes to cloned repository');
+
+console.log('Thirs change');
 
 console.log('Thirs change');
 
