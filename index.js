817c94d64b6ca0594e8573fd6f2e760aaa2760c4
vinutha91 <vinu.raju@gmail.com>
2019-08-05 12:29:05 +0530

    [CC-1106] | Login | Created a login form for MCA

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log("Hello, welcome to Git Training!");
 console.log('These are my changes to cloned repository');
 
 console.log('Thirs change');
+
+function login() {
+    // make an API call to login service
+    return true;
+}
